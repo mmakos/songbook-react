659569455b53a3e0be8b7759f10f4ac8b04eb017
Michał Makoś <michal.makos@pitradwar.com>
2025-04-13 23:01:17 +0200

    Poprawiłem ikonę dla edytora

--- src/user/VerifiedUserTooltip.tsx
+++ src/user/VerifiedUserTooltip.tsx
@@ -5,19 +5,20 @@ import { UserType } from './user.types.ts';
 
 const VerifiedUserTooltip: FC<Omit<TooltipProps, 'title'> & Attributes & { userType?: UserType }> = ({
   userType,
+    children,
   ...props
 }) => {
   if (!userType) return;
 
   switch (userType) {
     case UserType.SITH:
-      return <BasicTooltip {...props} title="Sith Lord (a dokładnie Darth Vader) - właściciel i stwórca" />;
+      return <BasicTooltip {...props} span title="Sith Lord (a dokładnie Darth Vader) - właściciel i stwórca">{children}</BasicTooltip>;
     case UserType.JEDI:
-      return <BasicTooltip {...props} title="Rycerz Jedi - moderator" />;
+      return <BasicTooltip {...props} span title="Rycerz Jedi - moderator">{children}</BasicTooltip>;
     case UserType.DROID:
-      return <BasicTooltip {...props} title="Droid (dokładnie RD2D) - bot" />;
+      return <BasicTooltip {...props} span title="Droid (dokładnie RD2D) - bot">{children}</BasicTooltip>;
     case UserType.CLONE:
-      return <BasicTooltip {...props} title="Klon armii republiki - zweryfikowany użytkownik" />;
+      return <BasicTooltip {...props} span title="Klon armii republiki - zweryfikowany użytkownik">{children}</BasicTooltip>;
   }
 };
 
