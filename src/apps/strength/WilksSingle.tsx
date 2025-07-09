356496dd362463f6ebaf1c8e632fb086ca6e0852
Michał Makoś <michal.makos@pitradwar.com>
2025-07-09 15:44:51 +0200

    Zacząłem dodawać trójbój do kalkulatora siły

--- src/apps/strength/WilksSingle.tsx
+++ src/apps/strength/WilksSingle.tsx
@@ -24,10 +24,24 @@ export interface ILifter {
   name: string;
   sex: TSex;
   bodyWeight: string;
+
+  bench: number;
+  benchInput: string;
+  benchReps: number;
+  benchRepMax: number;
+
+  squat: number;
+  squatInput: string;
+  squatReps: number;
+  squatRepMax: number;
+
+  deadlift: number;
+  deadliftInput: string;
+  deadliftReps: number;
+  deadliftRepMax: number;
+
   liftedWeight: number;
-  liftedWeightInput: string;
-  repMax: number;
-  reps: number;
+
   wilksCoefficient: number;
   wilks: number;
   wilksInput: string;
@@ -47,7 +61,7 @@ const WilksSingle: FC<IWilksSingleProps> = ({ units, method, lifter, updateLifte
 
   useMemo(() => {
     lifter.wilksCoefficient = calculateWilks(toKg(+lifter.bodyWeight, units), lifter.sex, method);
-    lifter.wilks = lifter.wilksCoefficient * toKg(lifter.repMax, units);
+    lifter.wilks = lifter.wilksCoefficient * toKg(lifter.liftedWeight, units);
     lifter.wilksInput = lifter.wilks.toFixed(3);
   }, [method]);
 
@@ -56,20 +70,20 @@ const WilksSingle: FC<IWilksSingleProps> = ({ units, method, lifter, updateLifte
     lifter.units = units;
     if (units === 'kg') {
       lifter.bodyWeight = normalize(lbsToKg(+lifter.bodyWeight), 1, 500).toFixed(1);
-      lifter.liftedWeight = normalize(lbsToKg(lifter.liftedWeight), 1, 2000);
+      lifter.squat = normalize(lbsToKg(lifter.squat), 1, 2000);
     } else {
       lifter.bodyWeight = normalize(kgToLbs(+lifter.bodyWeight), 1, 1000).toFixed(1);
-      lifter.liftedWeight = normalize(kgToLbs(lifter.liftedWeight), 1, 5000);
+      lifter.squat = normalize(kgToLbs(lifter.squat), 1, 5000);
     }
-    lifter.liftedWeightInput = lifter.liftedWeight.toFixed(1);
-    lifter.repMax = epleyRepMax(lifter.reps, lifter.liftedWeight);
+    lifter.squatInput = lifter.squat.toFixed(1);
+    lifter.liftedWeight = epleyRepMax(lifter.reps, lifter.squat);
   }, [units]);
 
-  const { bodyWeight, sex, liftedWeightInput, wilksCoefficient, wilksInput, name, reps, repMax } = lifter;
+  const { bodyWeight, sex, squatInput, wilksCoefficient, wilksInput, name, reps, liftedWeight } = lifter;
 
   const updateLifterProp = (props: Partial<ILifter>) => {
-    if (props.liftedWeightInput !== undefined) {
-      props.liftedWeight = +props.liftedWeightInput;
+    if (props.benchInput !== undefined) {
+      props.bench = +props.benchInput;
     } else if (props.wilksInput !== undefined) {
       props.wilks = +props.wilksInput;
     }
@@ -77,17 +91,17 @@ const WilksSingle: FC<IWilksSingleProps> = ({ units, method, lifter, updateLifte
     const l: ILifter = { ...lifter, ...props };
 
     if (props.wilks !== undefined) {
-      l.repMax = toUnits(l.wilks / l.wilksCoefficient, units);
-      l.liftedWeight = epleyRepWeight(l.reps, l.repMax);
-      l.liftedWeightInput = l.liftedWeight.toFixed(1);
+      l.liftedWeight = toUnits(l.wilks / l.wilksCoefficient, units);
+      l.squat = epleyRepWeight(l.reps, l.liftedWeight);
+      l.squatInput = l.squat.toFixed(1);
     } else {
-      if (props.reps !== undefined || props.liftedWeight !== undefined) {
-        l.repMax = epleyRepMax(l.reps, l.liftedWeight);
+      if (props.reps !== undefined || props.bench !== undefined) {
+        l.liftedWeight = epleyRepMax(l.reps, l.squat);
       }
       if (props.bodyWeight !== undefined || props.sex !== undefined) {
         l.wilksCoefficient = calculateWilks(toKg(+l.bodyWeight, units), l.sex, method);
       }
-      l.wilks = l.wilksCoefficient * toKg(l.repMax, units);
+      l.wilks = l.wilksCoefficient * toKg(l.liftedWeight, units);
       l.wilksInput = l.wilks.toFixed(3);
     }
 
@@ -97,7 +111,12 @@ const WilksSingle: FC<IWilksSingleProps> = ({ units, method, lifter, updateLifte
   return (
     <Stack spacing={2}>
       <Stack direction="row" spacing={1}>
-        <TextField label="Osoba" value={name} onChange={(event) => updateLifterProp({ name: event.target.value })} />
+        <TextField
+          label="Osoba"
+          value={name}
+          onChange={(event) => updateLifterProp({ name: event.target.value })}
+          fullWidth
+        />
         <ToggleButtonGroup value={sex} onChange={(_, v) => v && updateLifterProp({ sex: v as TSex })} exclusive>
           <BasicTooltip title="Mężczyzna">
             <ToggleButton value="male">
@@ -120,32 +139,105 @@ const WilksSingle: FC<IWilksSingleProps> = ({ units, method, lifter, updateLifte
           input: { endAdornment: <InputAdornment position="end">{units}</InputAdornment> },
         }}
         onFocus={(event) => event.target.select()}
+        required
       />
-      <NumberField
-        label="Podniesiony ciężar"
-        value={liftedWeightInput}
-        onChange={(event) => updateLifterProp({ liftedWeightInput: event.target.value })}
-        slotProps={{
-          htmlInput: { min: 1, max: units === 'kg' ? 2000 : 5000, step: units === 'kg' ? 0.5 : 1 },
-          input: { endAdornment: <InputAdornment position="end">{units}</InputAdornment> },
-        }}
-        onFocus={(event) => event.target.select()}
-      />
-      <NumberField
-        label="Powtórzenia"
-        value={reps}
-        onChange={(event) => updateLifterProp({ reps: +event.target.value })}
-        slotProps={{
-          htmlInput: { min: 1, max: 50, step: 1 },
-        }}
-        onFocus={(event) => event.target.select()}
-      />
+      <Stack direction="row" spacing={1}>
+        <Stack spacing={2} width="100%">
+          <Divider>
+            <Typography variant="body2">Ławka</Typography>
+          </Divider>
+          <NumberField
+            label="Ciężar"
+            value={lifter.benchInput}
+            slotProps={{
+              htmlInput: { min: 1, max: units === 'kg' ? 2000 : 5000, step: units === 'kg' ? 0.5 : 1 },
+              input: { endAdornment: <InputAdornment position="end">{units}</InputAdornment> },
+            }}
+            onFocus={(event) => event.target.select()}
+          />
+          <NumberField
+            label="Powtórzenia"
+            value={lifter.benchReps}
+            slotProps={{
+              htmlInput: { min: 1, max: 50, step: 1 },
+            }}
+            onFocus={(event) => event.target.select()}
+          />
+        </Stack>
+        <Stack spacing={2} width="100%">
+          <Divider>
+            <Typography variant="body2">Przysiad</Typography>
+          </Divider>
+          <NumberField
+            label="Ciężar"
+            value={lifter.squatInput}
+            slotProps={{
+              htmlInput: { min: 1, max: units === 'kg' ? 2000 : 5000, step: units === 'kg' ? 0.5 : 1 },
+              input: { endAdornment: <InputAdornment position="end">{units}</InputAdornment> },
+            }}
+            onFocus={(event) => event.target.select()}
+          />
+          <NumberField
+            label="Powtórzenia"
+            value={lifter.squatReps}
+            slotProps={{
+              htmlInput: { min: 1, max: 50, step: 1 },
+            }}
+            onFocus={(event) => event.target.select()}
+          />
+        </Stack>
+        <Stack spacing={2} width="100%">
+          <Divider>
+            <Typography variant="body2">Martwy</Typography>
+          </Divider>
+          <NumberField
+            label="Ciężar"
+            value={lifter.deadliftInput}
+            slotProps={{
+              htmlInput: { min: 1, max: units === 'kg' ? 2000 : 5000, step: units === 'kg' ? 0.5 : 1 },
+              input: { endAdornment: <InputAdornment position="end">{units}</InputAdornment> },
+            }}
+            onFocus={(event) => event.target.select()}
+          />
+          <NumberField
+            label="Powtórzenia"
+            value={lifter.deadliftReps}
+            slotProps={{
+              htmlInput: { min: 1, max: 50, step: 1 },
+            }}
+            onFocus={(event) => event.target.select()}
+          />
+        </Stack>
+      </Stack>
       <Divider>
         <Typography variant="body2">Wyniki</Typography>
       </Divider>
+      <Stack direction="row" spacing={1}>
+        <TextField
+          label="Ławka"
+          value={lifter.benchRepMax}
+          slotProps={{
+            input: { readOnly: true, endAdornment: <InputAdornment position="end">{units}</InputAdornment> },
+          }}
+        />
+        <TextField
+          label="Przysiad"
+          value={lifter.squatRepMax}
+          slotProps={{
+            input: { readOnly: true, endAdornment: <InputAdornment position="end">{units}</InputAdornment> },
+          }}
+        />
+        <TextField
+          label="Marwty"
+          value={lifter.deadliftRepMax}
+          slotProps={{
+            input: { readOnly: true, endAdornment: <InputAdornment position="end">{units}</InputAdornment> },
+          }}
+        />
+      </Stack>
       <TextField
-        label="Makymalny ciężar"
-        value={repMax.toFixed(1)}
+        label="Łączny ciężar"
+        value={liftedWeight.toFixed(1)}
         slotProps={{ input: { readOnly: true, endAdornment: <InputAdornment position="end">{units}</InputAdornment> } }}
       />
       <TextField
